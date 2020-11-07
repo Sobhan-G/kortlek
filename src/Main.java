2ef6620be996288cdc07fd23deae5930e03abaab
Sobhan <Gilani.sobhan@gmail.com>
2020-11-07 15:36:31 +0100

    ändring 1

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,15 @@ public class Main {
         Kortlek kortlek=new Kortlek();
         kortlek.gorKortlek();
         kortlek.blanda();
-        kortbunt k=kortlek.geOversta();
-        System.out.println(k.toString());
+        kortbunt k1=kortlek.geOversta();
+        System.out.println(k1.geFarg()+k1.geValor());
+        kortbunt k2=kortlek.geOversta();
+        System.out.println(k2.geFarg()+k2.geValor());
+
+        if(k1.geValor() > k2.geValor())
+            System.out.println("spelare 1 vann");
+                else if (k2.geValor()< k1.geValor())
+                    System.out.println("spelare 1 vann");
+                         else System.out.println("Oavgjort, spela igen?");
     }
 }
