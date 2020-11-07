efec7ff44d277aa36e29f7d7124030f9eb879f75
Sobhan <Gilani.sobhan@gmail.com>
2020-11-07 15:16:24 +0100

    ändring 1

--- src/Kortlek.java
+++ src/Kortlek.java
@@ -1,32 +1,32 @@
 
 public class Kortlek  {
+        // en kortbunt 52 platser i array
     public final kortbunt[] bunt=new kortbunt[52];
     public int antal=0;
 
     public int geAntal() {
         return antal;
     }
-
+            // lägg korten överst
     public void laggOverst(kortbunt k) {
         bunt[antal]=k;
         antal++;
     }
-
+        // ge de översta kortet
     public kortbunt geOversta() {
         antal=0;
         return bunt[antal];
     }
-
+            // innehlleet i kortleken
     public void gorKortlek() {
         for (int f=1; f <= 4; f++)
             for (int v=1; v <= 13; v++)
                 laggOverst(new kortbunt(f, v));
     }
-
-    public void blanda() { 
+            // blanda genom math.random
+    public void blanda() {
         for (int i=0; i < antal; i++) {
-            int n=i + (int) (Math.random() * antal);
-            i++;
+            int n= (int) (Math.random() * antal);
             kortbunt temp;
             temp=bunt[n];
             bunt[n]=bunt[i];
--- src/kortbunt.java
+++ src/kortbunt.java
@@ -1,10 +1,12 @@
+    // en metod som hanterar en kortbunt
 
 public class kortbunt {
+     // defineria olka "färgnnamnen"
     public static final String[] fargNamn={"Hjarter", "Spader", "Ruter", "Klover"};
-
+            // definera valör
     public static final String[] valorNamn={"Ess", "2", "3", "4", "5", "6", "7",
             "8", "9", "10", "Knekt", "Dam", "Kung"};
-
+        //
     public final int f;
     public final int v;
 
