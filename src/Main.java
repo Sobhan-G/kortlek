083bbb007ce612021ab7a3ee6fda0d94d481e9c9
Sobhan <Gilani.sobhan@gmail.com>
2020-11-09 21:19:25 +0100

    slutgiltiga ändringar 1

--- src/Main.java
+++ src/Main.java
@@ -7,13 +7,13 @@ public class Main {
         kortlek.blanda();
         kortbunt p1=kortlek.slumpkort();
         kortbunt p2= kortlek.slumpkort();
-        System.out.println("Spelare_1"+ " "+ p1.toString());
-        System.out.println("Spelare_2"+ " "+ p2.toString());
+        System.out.println("Spelare1"+ " "+ p1.toString());
+        System.out.println("Spelare2"+ " "+ p2.toString());
 
         if (p1.v > p2.v)
-            System.out.println("Spelare 1 vann");
+            System.out.println("Spelare1 Du Vann!");
         else if (p2.v > p1.v)
-            System.out.println("Spelare 2 vann!");
+            System.out.println("Spelare2 Du Vann!");
         else System.out.println("Oavgjort!");
         System.out.println("TACK FÖR ATT DU SPELADE!");
 
